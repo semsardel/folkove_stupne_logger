fd991c384352298a0b1e9a86b92f983f3356f9ee
teevee <semsardel@gmail.com>
2021-02-07 21:26:58 +0100

    Update hlasy.py
    fix

--- scripts/python/hlasy.py
+++ scripts/python/hlasy.py
@@ -2,64 +2,64 @@ import urllib.request
 import time
 import datetime
 
-# FILENAME = 'hlasy.txt'
-# TIMER = 120    # timer interval [sec], minimum 10 sec
+FILENAME = 'hlasy10s.txt'
 
-FILENAME = 'hlasy_10s.txt'
-TIMER = 10    # timer interval [sec], minimum 10 sec
+TIMER = 10    # [sec]
 
 def get_url(url:str, filename:str):
-    try:
-        # print("getUrl: {}".format(url))
-        fp = urllib.request.urlopen(url, timeout=10)
-        # print("read...")
-        content = fp.read()
-        if filename != None:
-            # save data
-            # print("write file: {}".format(filename))
-            try:
-              newFile = open(filename, "wb")
-              newFile.write(content)
-              newFile.close()
-            except Exception as e:
-                print('Write file exception: {}'.format(e))
-        return content
-    except Exception as e:
-        print('getUrl exception:')
-        print(e)
-        return ''
+    fp = urllib.request.urlopen(url, timeout=10)
+    content = fp.read()
+    if filename != None:
+        try:
+            newFile = open(filename, "wb")
+            newFile.write(content)
+            newFile.close()
+        except Exception as e:
+            print('Write file exception: {}'.format(e))
+    return content
 
 
 def parse_hlasy():
-   try: 
-     wc = str(get_url('http://www.countryradio.cz/folkove-stupne/', None)).encode().decode('unicode-escape')
-     wc.replace('\t','').replace('\n','')
-     d1 = wc.split('<h3 class="nadpis size_18">Folkov')[1].split('</table>')[0]
-     d2 = d1.split('<td class="size_11 c">')
-     d3 = list(map(lambda x : x.split('</td>')[0], d2[1:]))
-     return d3
-   except Exception as e:
-     print('Exception: parse_hlasy:')
-     print(e)
-     return ''  
+    wc = str(get_url('http://www.countryradio.cz/folkove-stupne/', None)).encode().decode('unicode-escape')
+    wc.replace('\t','').replace('\n','')
+    d1 = wc.split('<h3 class="nadpis size_18">Folkov')[1].split('</table>')[0]
+    d2 = d1.split('<td class="size_11 c">')
+    d3 = list(map(lambda x : int(x.split('</td>')[0]), d2[1:]))
+    return d3
 
 
-while True:
-  try:  
-    num = parse_hlasy()
-    dt = datetime.datetime.today()
-    stamp = '{}-{}-{}_{:02}:{:02}:{:02} '.format(dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second)
-    votelog = '{}{}\r\n'.format(stamp, num)
-    print(votelog)
-    with open(FILENAME, 'a') as file:
-      file.write(votelog)
-  except Exception as e:
-    print('Global Exception:')
-    print(e)
-  print('Timer: ', end='')
-  for i in range(int(TIMER / 10)):
-    print('{}'.format(i%10), end='')  
-    time.sleep(10)
-  print('')
-  
-      
+if __name__ == '__main__':
+  while True:
+    try: 
+      try:  
+        print('Getting data...')
+        num = []
+        try:
+          num = parse_hlasy()
+        except Exception as e:
+          print('EXCEPTION: parse exception: ')
+          print(e)
+          num = []    # if error create empty row
+        dt = datetime.datetime.today()
+        stamp = '{}-{:02}-{:02}_{:02}:{:02}:{:02}'.format(dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second)
+        vote = ';  '.join(map(lambda x: str(x), num))
+        votelog = '{};  {}\r\n'.format(stamp, vote)
+        print(votelog)
+        with open(FILENAME, 'a') as file:
+          file.write(votelog)
+            
+      except KeyboardInterrupt: 
+        raise KeyboardInterrupt
+        
+      except Exception as e:
+        print('Exception:')
+        print(e)
+        
+      time.sleep(TIMER)
+        
+    except KeyboardInterrupt: 
+      print("\n\nCtrl+c. FINISH.")
+      exit(0) 
+
+
+
